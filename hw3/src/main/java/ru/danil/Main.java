0af8ce009cf7b943d5b8d3109109033fc4c115a1
DanilFlint <didenkod2010@gmail.com>
2024-08-15 01:27:59 +0300

    Сообщения записываются

--- hw3/src/main/java/ru/danil/Main.java
+++ hw3/src/main/java/ru/danil/Main.java
@@ -1,7 +1,19 @@
 package ru.danil;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Utils.log.info("Hello");
+
+        var producer = new KafkaProducer<String, String>(Utils.producerConfig);
+        for (int i = 0; i < 2; i++) {
+            producer.send(new ProducerRecord<>("topic1", Integer.toString(i), Integer.toString(i)));
+
+        }
+
+        producer.close();
     }
 }
